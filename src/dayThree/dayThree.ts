84f9444c412d73d4e257fa21378af70bfad29eff
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-24 20:51:00 +1300

    feat(dayThree): :white_check_mark: solve part two

--- src/dayThree/dayThree.ts
+++ src/dayThree/dayThree.ts
@@ -1,4 +1,16 @@
-import readInput = require('../readInput')
+import readInput = require('../readInput');
+
+type Rating = 'oxygenGenerator' | 'carbonDioxideScrubber';
+type BitCritiera = (countOfOnes: number, countOfZeroes: number) => string;
+
+type RatingMap<T> = {
+	[rating in Rating]: T
+};
+
+const BIT_CRITERIA: RatingMap<BitCritiera> = {
+	oxygenGenerator: <BitCriteria>(countOfOnes: number, countOfZeroes: number): string => ((countOfOnes >= countOfZeroes) ? '1' : '0'),
+	carbonDioxideScrubber: <BitCriteria>(countOfOnes: number, countOfZeroes: number): string => ((countOfOnes >= countOfZeroes) ? '0' : '1'),
+};
 
 const report = readInput.string(__dirname);
 
@@ -9,22 +21,42 @@ const report = readInput.string(__dirname);
  * @returns {number} The number of occurences of `element` in `array`
  */
 const countOccurences = (array: any[], element: any): number => {
-	return array.reduce((count, _element) => count + ((_element === element) ? 1 : 0), 0)
+	return array.reduce((count, _element) => count + ((_element === element) ? 1 : 0), 0);
+}
+
+/**
+ * Apply a specified \<BitCriteria> to a specified \<string[]> of position bits and return the bit to keep
+ * @param {string[]} positionBits A \<string[]> of the single bits at the same index of all binary numbers in the report
+ * @param {BitCritiera} bitCriteria The \<BitCriteria> function to apply
+ * @returns {string} The bit to keep as per the bit criteria
+ */
+const applyBitCriteria = (positionBits: string[], bitCriteria: BitCritiera): string => {
+	return bitCriteria(countOccurences(positionBits, '1'), countOccurences(positionBits, '0'));
 }
 
 const bitLength = report[0].length;
 
-let [gammaRate, epsilonRate] = ['', ''];
+const ratings: RatingMap<string[]> = {
+	'oxygenGenerator': [...report],
+	'carbonDioxideScrubber': [...report],
+};
 
-// Calculate the gamma rate and the epsilon rate
 for (let index = 0; index < bitLength; index++) {
-	const positionBits = report.map(binary => binary[index])
+	// Iterate for each required rating
+	Object.entries(ratings).forEach(([rating, binaries]) => {
+		if (binaries.length === 1) return;
 
-	const countOfOnes = countOccurences(positionBits, '1');
-	const countOfZeros = countOccurences(positionBits, '0');
+		const positionBits = binaries.map(binary => binary[index]);
 
-	gammaRate += (countOfOnes > countOfZeros) ? '1' : '0';
-	epsilonRate += (countOfOnes > countOfZeros) ? '0' : '1';
+		// Apply the respective <BitCriteria> to the position bits at the current index,
+		// and filter the rating's binaries array for the numbers which match
+		ratings[<Rating>rating] = binaries.filter(binary => {
+			return binary[index] === applyBitCriteria(positionBits, BIT_CRITERIA[<Rating>rating]);
+		});
+	})
+
+	// Break if all the ratings have been found
+	if (Object.values(ratings).every(binaries => binaries.length === 1)) break;
 }
 
 /**
@@ -34,6 +66,6 @@ for (let index = 0; index < bitLength; index++) {
  */
 const binaryToDecimal = (binary: string): number => parseInt(binary, 2);
 
-const powerConsumption = binaryToDecimal(gammaRate) * binaryToDecimal(epsilonRate);
+const lifeSupportRating = binaryToDecimal(ratings.oxygenGenerator[0]) * binaryToDecimal(ratings.carbonDioxideScrubber[0]);
 
-console.log(powerConsumption);
+console.log(lifeSupportRating);
