7f2e23d323b9d8db5e8d93d59ebb6e0b7d12c34d
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-24 16:21:00 +1300

    feat(readInput): :sparkles: commit readInput module
    adapted from JamesNZL/AoC-2020@19c9728

---
+++ src/readInput.ts
@@ -0,0 +1,24 @@
+import fs = require('fs');
+
+/**
+ * Read a specified file and return it as a \<string[]>
+ * @param {string} dirname The directory of the input file to read
+ * @param {boolean} [isGrouped=false] Whether the input file is grouped by newlines
+ * @returns {string[]} The read file as a \<string[]>
+ */
+export const string = (dirname: string, isGrouped: boolean = false): string[] => {
+	const input = fs.readFileSync(`${dirname}\\input.txt`, { encoding: 'utf-8', flag: 'r' })
+		.trim();
+
+	return (isGrouped)
+		? input.split('\n\n')
+		: input.split('\n');
+};
+
+/**
+ * Read a specified file and return it as a \<number[]>
+ * @param {string} dirname The directory of the input file to read
+ * @param {boolean} [isGrouped=false] Whether the input file is grouped by newlines
+ * @returns {number[]} The read file as a \<number[]>
+ */
+export const number = (dirname: string, isGrouped: boolean = false): number[] => string(dirname, isGrouped).map(Number)
