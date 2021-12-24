1f9475a993fa47122e9fc0976f03b00b2a41e0ee
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-24 17:06:45 +1300

    feat(dayOne): :white_check_mark: solve part two

--- src/dayOne/dayOne.ts
+++ src/dayOne/dayOne.ts
@@ -2,6 +2,12 @@ import readInput = require('../readInput');
 
 const depths = readInput.number(__dirname);
 
-const numberOfIncreases = depths.reduce((count, depth, index) => count + ((depth > depths?.[--index]) ? 1 : 0), 0);
+const slidingWindows = depths.flatMap((depth, index) => {
+	return (depths?.[index + 2] === undefined)
+		? []
+		: [depth + depths[++index] + depths[++index]];
+})
+
+const numberOfIncreases = slidingWindows.reduce((count, window, index) => count + ((window > slidingWindows?.[--index]) ? 1 : 0), 0);
 
 console.log(numberOfIncreases);
