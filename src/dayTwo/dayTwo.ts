e1daed32a0a11e5742be77a99087bef2656f5001
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-24 17:33:33 +1300

    feat(dayTwo): :white_check_mark: solve part two

--- src/dayTwo/dayTwo.ts
+++ src/dayTwo/dayTwo.ts
@@ -3,7 +3,7 @@ import readInput = require('../readInput')
 type CommandInstruction = 'forward' | 'down' | 'up';
 type Command = [CommandInstruction, number]
 
-const DEPTH_COMMANDS = {
+const AIM_COMMANDS = {
 	'down': 1,
 	'up': -1,
 }
@@ -15,11 +15,16 @@ const commands: Command[] = readInput.string(__dirname)
 const submarinePosition = {
 	horizontal: 0,
 	depth: 0,
+	aim: 0,
 };
 
 commands.forEach(([instruction, value]) => {
-	if (instruction === 'forward') submarinePosition.horizontal += value;
-	else if (Object.keys(DEPTH_COMMANDS).includes(instruction)) submarinePosition.depth += (value * DEPTH_COMMANDS[instruction]);
+	if (instruction === 'forward') {
+		submarinePosition.horizontal += value;
+		submarinePosition.depth += (submarinePosition.aim * value)
+	}
+
+	else if (Object.keys(AIM_COMMANDS).includes(instruction)) submarinePosition.aim += (value * AIM_COMMANDS[instruction]);
 })
 
 console.log(submarinePosition.horizontal * submarinePosition.depth)
