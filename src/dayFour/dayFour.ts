4c4f97343f6f4f2d03e28bf4d051623560c9a60f
James <44986932+JamesNZL@users.noreply.github.com>
2022-01-01 12:34:33 +1300

    feat(dayFour): :white_check_mark: solve part one

---
+++ src/dayFour/dayFour.ts
@@ -0,0 +1,57 @@
+import readInput = require('../readInput');
+
+const [rawNumbers, ...rawBoards] = readInput.string(__dirname, true);
+
+const numbers = rawNumbers.split(/,/g).map(Number);
+
+const boards = rawBoards.map(rawBoard => {
+	return rawBoard.split(/\n/g)
+		.map(row => {
+			return row.split(/(?<=\d) +/g)
+				.map(Number);
+		});
+});
+
+/**
+ * Check if all numbers in any of the rows of the specified \<number[][]> exist in `drawnNumbers`
+ * @param {number[][]} board The board \<number[][]> to check
+ * @param {number} drawnNumbers The \<number[]> of drawn numbers
+ * @returns {boolean} Whether any of the rows contain only drawn numbers
+ */
+const rowsMatch = (board: number[][], drawnNumbers: number[]): boolean => board.some(row => row.every(number => drawnNumbers.includes(number)));
+
+/**
+ * Transpose a board \<number[][]>
+ * @param {number[][]} board The board \<number[][]> to transpose
+ * @returns {number[][]} The transposed board
+ */
+const transposeBoard = (board: number[][]): number[][] => board[0].map((_, columnIndex) => board.map(row => row[columnIndex]));
+
+/**
+ * Check if the specified board \<number[][]> satisfies bingo
+ * @param {number[][]} board The board \<number[][]> to check
+ * @param {number[]} drawnNumbers The \<number[]> of drawn numbers
+ * @returns {boolean} Whether the board contains a bingo
+ */
+const checkIfBingo = (board: number[][], drawnNumbers: number[]): boolean => rowsMatch(board, drawnNumbers) || rowsMatch(transposeBoard(board), drawnNumbers);
+
+const calculateScore = (board: number[][], drawnNumbers: number[]): number => {
+	const lastNumber = <number>drawnNumbers.at(-1);
+	const sumOfUnmarked = board.flat()
+		.filter(number => !drawnNumbers.includes(number))
+		.reduce((sum, number) => sum + number, 0);
+
+	return lastNumber * sumOfUnmarked;
+};
+
+numberLoop:
+for (let index = 0; index < numbers.length; index++) {
+	for (const board of boards) {
+		const drawnNumbers = numbers.slice(0, index + 1);
+
+		if (checkIfBingo(board, drawnNumbers)) {
+			console.log(calculateScore(board, drawnNumbers));
+			break numberLoop;
+		}
+	}
+}
